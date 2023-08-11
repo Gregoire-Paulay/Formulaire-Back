a460931e36e93c2d40ef9b2b5320bfad2d609ad1
Grégoire Paulay <gregoirepaulay.pro@gmail;com>
2023-08-11 16:57:05 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+// Import des packages nécéssaire pour utiliser mailgun
+const formData = require("form-data");
+const Mailgun = require("mailgun.js");
+// -- Utilisation de Mailgun
+const mailgun = new Mailgun(formData);
+const client = mailgun.client({
+  username: "Grégoire Paulay",
+  key: process.env.MAILGUN_API_KEY,
+});
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+PORT = 3000;
+
+app.get("/", (req, res) => {
+  try {
+    return res.status(200).json("Bienvenue sur le formulaire!");
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+});
+
+app.post("/form", async (req, res) => {
+  try {
+    console.log(req.body);
+    const { firstname, lastname, email, sujet, message } = req.body;
+    const messageData = {
+      from: `${firstname} ${lastname} ${email}`,
+      to: "gregoire.paulay@gmail.com",
+      subject: sujet,
+      text: message,
+    };
+    const response = await client.messages.create(
+      process.env.MAILGUN_DOMAIN,
+      messageData
+    );
+
+    console.log(response);
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Toute les routes sauf celles crées au dessus arriveront ici
+app.all("*", (req, res) => {
+  return res.status(404).json("Not found");
+});
+//Pour écouter le serveur : ici on écoute la requete du port 3000
+app.listen(process.env.PORT, () => {
+  console.log(`server started on port : ${PORT}`);
+});
